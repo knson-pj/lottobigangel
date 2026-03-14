5fa1836dd50e14c78db8e444068752fae3385081
knson-pj <knson82@gmail.com>
2026-03-14 19:58:22 +0900

    Add files via upload

---
+++ app/api/predictions/latest/page.tsx
@@ -0,0 +1,10 @@
+import LatestPredictionPanel from "./latest-prediction-panel";
+
+export const metadata = {
+  title: "Latest Predictions",
+  description: "최신 로또 예측 스냅샷 확인 페이지",
+};
+
+export default function LatestPredictionsPage() {
+  return <LatestPredictionPanel />;
+}
